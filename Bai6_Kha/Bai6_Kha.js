0616e374992c457ab8e6f38f7f270ac8cdbbe69a
Pham-anh-Hub <phamphuonganhcutee@gmail.com>
2025-03-06 17:14:27 +0700

    update Bai6_Kha

--- Bai6_Kha/Bai6_Kha.js
+++ Bai6_Kha/Bai6_Kha.js
@@ -5,8 +5,12 @@ if(!Number.isNaN(n)){
     for(let i = 0; i < 10 ; i++){
         if(n === numbers[i])
             count++;
+    } 
+    if(count === 0){
+    console.log(`Số ${n} không xuất hiện trong mảng`);  
+    }else{
+        console.log(`Số ${n} xuất hiện ${count} lần`); 
     }
-    console.log(`Số ${n} xuất hiện ${count} lần`);  
 }else{
     alert(`Số nhập vào không hợp lệ`)
 }
